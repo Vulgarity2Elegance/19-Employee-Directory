aa6b8b2c0e0c2acbcffe9c6d33b5b55dd57a3224
Youzhi Hu <jyzhu0101@gmail.com>
2020-09-28 16:21:42 +1000

    Imported API from src/utils & imported container from material-ui.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+
+function App() {
+  return (
+    <div className="App">
+      <Container className="sm"></Container>
+    </div>
+  );
+}
+
+export default App;
