38c6cae595fa2a89cb276ee42e4a3c5f821d8b1a
Youzhi Hu <jyzhu0101@gmail.com>
2020-09-29 12:08:13 +1000

    Using props to populate the rows of the table.

---
+++ src/components/Results.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { TableRow, TableCell } from "@material-ui/core";
+
+//using props to populate the rows of the table
+function Results(props) {
+  return (
+    <TableRow>
+      <TableCell component="th" scope="row">
+        <img src={props.thumbnail} alt="thumbnail"></img>
+      </TableCell>
+      <TableCell align="center">{props.fullName}</TableCell>
+      <TableCell align="center">{props.phone}</TableCell>
+      <TableCell align="center">{props.email}</TableCell>
+      <TableCell align="center">{props.date}</TableCell>
+    </TableRow>
+  );
+}
+
+export default Results;
